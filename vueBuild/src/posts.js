dbf03036308540275e6ab3932cf265b1e4126e6c
Oleksandr <oleksandr_revchuk@epam.com>
2020-04-03 11:51:22 +0300

    added template for post data

---
+++ vueBuild/src/posts.js
@@ -0,0 +1,267 @@
+module.exports = [
+  {
+    img: 'assets/images/BedSet (1).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (5).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (9).jpg'
+      }
+    ],
+    additional: {
+      top: false,
+      new: false,
+      rating: 3
+    }
+  },{
+    img: 'assets/images/BedSet (2).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (11).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (1).jpg'
+      }
+    ],
+    additional: {
+      top: true,
+      new: false,
+      rating: 3
+    }
+  },{
+    img: 'assets/images/BedSet (3).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (10).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (11).jpg'
+      }
+    ],
+    additional: {
+      top: true,
+      new: false,
+      rating: 5
+    }
+  },{
+    img: 'assets/images/BedSet (4).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (7).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (6).jpg'
+      }
+    ],
+    additional: {
+      top: false,
+      new: false,
+      rating: 5
+    }
+  },{
+    img: 'assets/images/BedSet (5).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (2).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (3).jpg'
+      }
+    ],
+    additional: {
+      top: false,
+      new: false,
+      rating: 5
+    }
+  },{
+    img: 'assets/images/BedSet (6).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (2).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (1).jpg'
+      }
+    ],
+    additional: {
+      top: false,
+      new: false,
+      rating: 4
+    }
+  },{
+    img: 'assets/images/BedSet (7).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (11).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (1).jpg'
+      }
+    ],
+    additional: {
+      top: true,
+      new: false,
+      rating: 4
+    }
+  },{
+    img: 'assets/images/BedSet (8).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (10).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (7).jpg'
+      }
+    ],
+    additional: {
+      top: true,
+      new: true,
+      rating: 4
+    }
+  },{
+    img: 'assets/images/BedSet (9).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (11).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (1).jpg'
+      }
+    ],
+    additional: {
+      top: true,
+      new: true,
+      rating: 4
+    }
+  },{
+    img: 'assets/images/BedSet (10).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (3).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (4).jpg'
+      }
+    ],
+    additional: {
+      top: true,
+      new: true,
+      rating: 5
+    }
+  },{
+    img: 'assets/images/BedSet (11).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (2).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (12).jpg'
+      }
+    ],
+    additional: {
+      top: false,
+      new: true,
+      rating: 5
+    }
+  },{
+    img: 'assets/images/BedSet (12).jpg',
+    artikul: '1234',
+    title: 'Двоспальний Комплект постільної білизни "Камея"',
+    description: 'Красивий та зручний постільний набір',
+    price: {
+      value: '397.00',
+      currency: 'грн'
+    },
+    options: [
+      {
+        img: 'assets/images/BedSet (11).jpg'
+      },
+      {
+        img: 'assets/images/BedSet (1).jpg'
+      }
+    ],
+    additional: {
+      top: false,
+      new: true,
+      rating: 3
+    }
+  },
+]
